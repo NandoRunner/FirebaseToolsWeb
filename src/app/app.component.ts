83f6a406d5d2d129f049e749e67f32b248ae69e5
Fernando Andrade <nando.az@gmail.com>
2018-09-18 10:39:09 -0300

    Alteração do title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firebase-tools-web';
+  title = 'firebase-tools-web by FAndrade Tec Info';
 }
